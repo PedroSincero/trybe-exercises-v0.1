85be4d31f418cc9cb0e023dbcfb1f81c6cde949e
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-02-24 16:16:55 -0300

    adicionando exercicios nas pasta 3.2/3.3/3.4 & 4.1

---
+++ trybe-exercises/exercises/4.1/practice1.js
@@ -0,0 +1,9 @@
+const name = "Pedro Henrique";
+const birthCity = "Agua Branca - Piauí";
+let birthYear = "19/06/1999";
+birthYear = 2020;
+birthCity = "São Paulo - Diadema"
+//constante não se mexe, é estatico
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
